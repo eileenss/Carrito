e74b5fac20062b0dc0945c4e8b7944d7792bd238
augustogoga <augustogoga@gmail.com>
2023-04-14 15:22:39 -0300

    Se agrega la clase Sucursal con sus respectivos atributos y los atributos de la clase StockItem

--- Carrito-D/Carrito-D/Models/StockItem.cs
+++ Carrito-D/Carrito-D/Models/StockItem.cs
@@ -2,5 +2,8 @@
 {
     public class StockItem
     {
+        public Sucursal sucursal { get; set; }
+        public Producto Producto { get; set; }
+        public int Cantidad { get; set; }
     }
 }
