c889c6816c1cec12191a0ce626c63fba494ca157
Alesis Bartoletti <71200173+ABartoletti@users.noreply.github.com>
2023-04-22 03:27:32 -0300

    Configuraciones base del EP2

--- Carrito-D/Carrito-D/Models/Empleado.cs
+++ Carrito-D/Carrito-D/Models/Empleado.cs
@@ -1,9 +1,12 @@
-﻿namespace Carrito_D.Models
+﻿using Carrito_D.Helpers;
+using System.ComponentModel.DataAnnotations;
+
+namespace Carrito_D.Models
 {
     public class Empleado : Persona
     {
         [Required(ErrorMessage = ErrorMsg.Requerido)]
-        [Range(1000, 99999), ErrorMessage = ErrorMsg.Rango]
+        [Range(1000, 999999, ErrorMessage = ErrorMsg.Rango)]
         public int Legajo { get; set; }
 
     }
--- Carrito-D/Carrito-D/Models/Persona.cs
+++ Carrito-D/Carrito-D/Models/Persona.cs
@@ -1,18 +1,26 @@
-﻿namespace Carrito_D.Models
+﻿using Carrito_D.Helpers;
+using System.ComponentModel.DataAnnotations;
+
+namespace Carrito_D.Models
 {
     public class Persona
     {
         public int Id { get; set; }
         [Required(ErrorMessage = ErrorMsg.Requerido)]
-        [Range(100000, 99999999), ErrorMessage = ErrorMsg.Rango]
+        [Range(100000, 99999999, ErrorMessage = ErrorMsg.Rango)]
         //[DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:00.000.000")]
         public int DNI { get; set; }
 
         [Required(ErrorMessage = ErrorMsg.Requerido)]
         [StringLength(85, MinimumLength = 2, ErrorMessage = ErrorMsg.CantCaracteres)]
+        [RegularExpression(@"[a-zA-Z áéíóú 0-9]*", ErrorMessage = ErrorMsg.Alfanumerico)]
+        [Display(Name = "Usuario")]
         public string UserName { get; set; }
 
         [Required(ErrorMessage = ErrorMsg.Requerido)]
+        [StringLength(50, MinimumLength = 8, ErrorMessage = ErrorMsg.CantCaracteres)]
+        [DataType(DataType.Password)]
+        [Display (Name = "Contraseña")]
         public string Password { get; set; }
 
         [Required(ErrorMessage = ErrorMsg.Requerido)]
@@ -25,12 +33,19 @@
         [RegularExpression(@"[a-zA-Z áéíóú]*", ErrorMessage = ErrorMsg.SoloLetras)]
         public string Apellido { get; set; }
 
+        [DataType(DataType.PhoneNumber)]
         public int Telefono { get; set; }
-        
+
+        [RegularExpression(@"[a-zA-Z áéíóú 0-9]*", ErrorMessage = ErrorMsg.Alfanumerico)]
         public string Direccion { get; set; }
-        
+
+        [Required(ErrorMessage = ErrorMsg.Requerido)]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = ErrorMsg.Requerido)]
+        [Display (Name = "Fecha de alta")]
+        [DataType(DataType.DateTime)]
         public DateTime FechaAlta { get; set; } = DateTime.Now;
 
     }
