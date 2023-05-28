4b1724b8d99e4d7f8301938dde9d19707f6df134
eileenss <eileenssanchez97@gmail.com>
2023-05-28 19:54:04 -0300

    ViewModel EditPersona para los controllers de Persona, Empleado y Cliente

---
+++ Carrito-D/Carrito-D/ViewModels/EditPersona.cs
@@ -0,0 +1,20 @@
+﻿using Carrito_D.Helpers;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Carrito_D.ViewModels
+{
+    public class EditPersona
+    {
+        public int Id { get; set; } 
+
+        [RegularExpression(@"^(549?)?(11\d{8})|([23]\d{9})$", ErrorMessage = ErrorMsg.Invalido)]
+        [Display(Name = "Teléfono")]
+        public int? Telefono { get; set; }
+
+        [RegularExpression(@"[a-zA-Z áéíóú 0-9]*", ErrorMessage = ErrorMsg.Alfanumerico)]
+        [StringLength(100, MinimumLength = 1, ErrorMessage = ErrorMsg.CantCaracteres)]
+        [Display(Name = "Dirección")]
+        public string Direccion { get; set; }
+    }
+}
